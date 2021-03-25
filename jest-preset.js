087edf963190db22a4a53c7b3fe564ebfcb4dbe5
n.mikhaylov <nmikhaylov92@gmail.com>
2021-03-25 18:44:24 +0300

    fix jest-preset.js

--- jest-preset.js
+++ jest-preset.js
@@ -1,1 +1,5 @@
-module.exports = require('./preset/puppeteer-preset');
+module.exports = {
+  globalSetup: require.resolve('./preset/lib/setup.js'),
+  globalTeardown: require.resolve('./preset/lib/teardown.js'),
+  testEnvironment: require.resolve('./preset/lib/puppeteer_environment.js'),
+}
